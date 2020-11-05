e67c2f84dcb5a901d265d27d28f27af3a4cf6976
D3ikin <70885287+D3ikin@users.noreply.github.com>
2020-11-05 14:10:54 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+  $('.blog-slider__items').slick({
+    arrows: false,
+    dots: true,
+  });
+
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active')
+    $('.menu__btn').toggleClass('menu__btn--active')
+  });
+
+  var mixer = mixitup('.gallery__content');
+  
+});
